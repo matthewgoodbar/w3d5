aeb0b822e34a9f81183cbbc3650f4244d16ecd28
matthew <matthewgoodbar@gmail.com>
2022-10-21 16:24:18 -0700

    Implement initialize, self.valid_moves, new_move_positions

--- knights_trevails/knightpathfinder.rb
+++ knights_trevails/knightpathfinder.rb
@@ -1,7 +1,32 @@
 require_relative './polytreenode.rb'
 
 class KnightPathFinder
+    attr_accessor :considered_positions
+
     def initialize(initial_pos)
-        @root_node = PolyTreeNode.new(initial_pos)
+        if KnightPathFinder.within_board(initial_pos)
+            root_node = PolyTreeNode.new(initial_pos)
+        else
+            raise RuntimeError.new "Not a valid starting position"
+        end
+        @considered_positions = [initial_pos]
+    end
+
+    def self.valid_moves(pos)
+        additions = [[1,2],[-1,2],[1,-2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]
+        res = additions.map {|addition| [addition[0]+pos[0], addition[1]+pos[1]] }
+        res.reject! {|position| !KnightPathFinder.within_board(position)}
+        return res
+    end
+
+    def self.within_board(pos)
+        return false if pos.any? {|index| index > 7 || index < 0}
+        return true
+    end
+
+    def new_move_positions(pos)
+        valids = KnightPathFinder.valid_moves(pos)
+        @considered_positions += valids
+        return valids
     end
 end
